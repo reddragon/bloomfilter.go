4e0f129898be2fa33d9e5bfa873afdb853b803e0
Gaurav <gaurav.menghani@gmail.com>
2012-11-05 12:24:10 -0500

    Added an initializer for the Bloom Filter

--- bf.go
+++ bf.go
@@ -2,10 +2,19 @@ package main
 import "fmt"
 
 type BloomFilter struct {
+	// TODO use a more conservative data-type like 'bit' for bitmap
 	bitmap []int // The bloom-filter bitmap
 	k int // The number of hash functions
 	n int // Number of elements in the filter
 	m int // Size of the bloom filter
+}
+
+func newBloomFilter(k, m int) *BloomFilter {
+	bf := new (BloomFilter)
+	bf.bitmap = make([]int, m)
+	bf.k, bf.m = k, m
+	bf.n = 0
+	return bf
 }
 
 // TODO Fill this
@@ -19,4 +28,6 @@ func (bf *BloomFilter) check(x int64) bool {
 
 func main() {
 	fmt.Println("Testing out the Bloom Filter")
+	bf := newBloomFilter(10, 20)
+	fmt.Println(bf)
 }
